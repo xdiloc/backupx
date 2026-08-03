acb762643868fbb6ec570e7a59b1fc4952b74631
xdiloc <xdiloc@yandex.ru>
2026-08-04 02:34:29 +0300

    up

--- release.sh
+++ release.sh
@@ -2,7 +2,9 @@
 
 NAME="backupx_linux_amd64"
 
-# Упаковываем файлы в архив. -j убирает пути, чтобы файлы лежали в корне zip.
+# Флаг -j убирает пути для основных файлов (они упаковываются в корень),
+# а папку asset добавляем через -r (она сохранится как папка asset/ в архиве)
 zip -j out/$NAME.zip out/backupx README.md LICENSE
+zip -r out/$NAME.zip asset
 
 echo "Готово: out/$NAME.zip"
